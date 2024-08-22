f526dfe4f619678e2d6a177397a0ba441e2ea346
Filbert Fernandes <filbertfernandes.bot@gmail.com>
2024-08-22 19:47:49 +0700

    Change filter buttons text color on Leaderboard Interface

--- src/components/interfaces/GameLeaderboardInterface.jsx
+++ src/components/interfaces/GameLeaderboardInterface.jsx
@@ -11,16 +11,16 @@ const GameLeaderboardInterface = () => {
 
     return (
         <div className={ `flex flex-col items-center gap-6 w-full h-full mt-4 ${isVisible ? 'animate-bounceIn' : 'opacity-0'}` }>
-            <div className="flex justify-between w-[90%] h-10 bg-stone-800/50 px-4 rounded-lg text-md">
+            <div className="flex justify-between text-sky-100 w-[90%] h-10 bg-stone-800/50 px-4 rounded-lg text-md">
                 <div className="flex items-center text-center border-sky-400 border-b-2" onClick={ () => console.log('clicked') } >
                     Ngoko
                 </div>
 
-                <div className="flex items-center text-center border-sky-400 border-b-2" onClick={ () => console.log('clicked') } >
+                <div className="flex items-center text-center" onClick={ () => console.log('clicked') } >
                     Krama Madya
                 </div>
 
-                <div className="flex items-center text-center border-sky-400 border-b-2" onClick={ () => console.log('clicked') } >
+                <div className="flex items-center text-center" onClick={ () => console.log('clicked') } >
                     Krama Alus
                 </div>
             </div>
