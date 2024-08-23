91c39dbe6e889500a7b46275da6a40eef071c4b3
Filbert Fernandes <filbertfernandes.bot@gmail.com>
2024-08-23 20:40:26 +0700

    Add responsiveness on GameMenuInterface

--- src/components/interfaces/GameMenuInterface.jsx
+++ src/components/interfaces/GameMenuInterface.jsx
@@ -23,31 +23,31 @@ const GameMenuInterface = ({ startGame, title }) => {
 
     return (
         <div className={ `flex flex-col justify-center items-center gap-6 w-full h-[75%] sm:h-full ${isVisible ? 'animate-bounceIn' : 'opacity-0'}` }>
-            <h1 className="text-4xl text-sky-400 drop-shadow-lg font-bold">{ title }</h1>
+            <h1 className="text-4xl text-sky-400 drop-shadow-lg font-bold md:text-5xl lg:text-6xl">{ title }</h1>
 
             <button 
-                className="p-1 bg-stone-800/50 w-28 text-sm text-white font-semibold rounded-lg shadow-md"
+                className="p-1 bg-stone-800/50 w-28 text-sm text-white font-semibold rounded-lg shadow-md md:w-36 md:text-2xl lg:p-1.5 lg:w-52 lg:text-3xl"
                 onClick={ () => startGame({ mode: 'ngoko' }) } onKeyDown={ handleKeyDown }
             >
                 Ngoko
             </button>
 
             <button 
-                className="p-1 bg-stone-800/50 w-28 text-sm text-white font-semibold rounded-lg shadow-md"
+                className="p-1 bg-stone-800/50 w-28 text-sm text-white font-semibold rounded-lg shadow-md md:w-36 md:text-2xl lg:p-1.5 lg:w-52 lg:text-3xl"
                 onClick={ () => startGame({ mode: 'madya' }) } onKeyDown={ handleKeyDown } 
             >
                 Krama Madya
             </button>
 
             <button 
-                className="p-1 bg-stone-800/50 w-28 text-sm text-white font-semibold rounded-lg shadow-md"
+                className="p-1 bg-stone-800/50 w-28 text-sm text-white font-semibold rounded-lg shadow-md md:w-36 md:text-2xl lg:p-1.5 lg:w-52 lg:text-3xl"
                 onClick={ () => startGame({ mode: 'alus' }) } onKeyDown={ handleKeyDown }
             >
                 Krama Alus
             </button>
 
             <button 
-                className="p-1 bg-stone-800/50 w-28 text-sm text-white font-semibold rounded-lg shadow-md"
+                className="p-1 bg-stone-800/50 w-28 text-sm text-white font-semibold rounded-lg shadow-md md:w-36 md:text-2xl lg:p-1.5 lg:w-52 lg:text-3xl"
                 onClick={ goToHome } onKeyDown={ handleKeyDown }
             >
                 Back to Home
