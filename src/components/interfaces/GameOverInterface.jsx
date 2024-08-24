4e63c0e839d578ceb48023bd7fba51bb641c9e8a
Filbert Fernandes <filbertfernandes.bot@gmail.com>
2024-08-24 19:26:29 +0700

    Add consistency in styling for GameOverInterface

--- src/components/interfaces/GameOverInterface.jsx
+++ src/components/interfaces/GameOverInterface.jsx
@@ -7,13 +7,13 @@ const GameOverInterface = ({ score, startGame, goToMenu }) => {
     }
 
     return (
-        <div className="flex flex-col justify-center items-center gap-6 w-full h-[75%]">
+        <div className="flex flex-col justify-center items-center gap-6 w-full h-[90%]">
             <div className="text-center">
-                <h1 className='text-4xl text-sky-400 drop-shadow-lg font-bold'>CONGRATULATIONS!</h1>
-                <h5 className='text-xl text-sky-400 drop-shadow-lg font-bold'>Your score is { score }</h5>
+                <h1 className='text-4xl text-sky-400 drop-shadow-lg font-bold lg:text-6xl'>CONGRATULATIONS!</h1>
+                <h5 className='text-xl text-sky-400 drop-shadow-lg font-bold lg:text-3xl'>Your score is { score }</h5>
             </div>
-            <button className='p-1 bg-stone-800/50 w-28 text-sm text-sky-100 font-semibold rounded-lg shadow-md' onClick={ startGame } onKeyDown={ handleKeyDown } >Retry</button>
-            <button className='p-1 bg-stone-800/50 w-28 text-sm text-sky-100 font-semibold rounded-lg shadow-md' onClick={ goToMenu } onKeyDown={ handleKeyDown } >Back to Menu</button>
+            <button className='p-1 bg-stone-800/50 w-28 text-sm text-white font-semibold rounded-lg shadow-md lg:p-1.5 lg:w-52 lg:text-3xl' onClick={ startGame } onKeyDown={ handleKeyDown } >Retry</button>
+            <button className='p-1 bg-stone-800/50 w-28 text-sm text-white font-semibold rounded-lg shadow-md lg:p-1.5 lg:w-52 lg:text-3xl' onClick={ goToMenu } onKeyDown={ handleKeyDown } >Back to Menu</button>
         </div>
     )
 }
