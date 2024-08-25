bba5dda41ec322d097c92a1eb1af0ecbc123d121
Filbert Fernandes <filbertfernandes.bot@gmail.com>
2024-08-25 19:09:58 +0700

    Add styling and responsiveness for GameMaterialInterface cards

--- src/components/interfaces/GameMaterialInterface.jsx
+++ src/components/interfaces/GameMaterialInterface.jsx
@@ -17,33 +17,33 @@ const GameMaterialInterface = ({ words }) => {
 
             <div className="flex flex-wrap gap-4 justify-evenly w-full h-full px-4 text-white overflow-y-auto sm:px-0 sm:gap-1">
                 {words.map((word, index) => (
-                    <div key={ index } className="flex justify-between items-center w-full h-36 bg-stone-800/50 rounded-[1rem] mb-2 pl-4 sm:w-[45%] lg:w-[30%]">
+                    <div key={ index } className="flex justify-between items-center w-full h-36 bg-stone-800/50 rounded-[1rem] mb-2 pl-4 sm:w-[45%] lg:w-[30%] lg:h-40">
                         <img 
                             src="https://img.freepik.com/free-vector/hand-drawn-eyeball-cartoon-illustration_23-2150873727.jpg" 
                             alt="Logo" 
                             className="w-24 h-24 object-cover rounded-[1rem]" 
                         />
-                        <div className="flex flex-col w-full h-full px-4 py-4">
+                        <div className="flex flex-col w-full h-full p-4">
                             <div className="text-2xl underline mb-2">{ word.indonesian }</div>
-                            <div className="flex justify-between text-base">
+                            <div className="flex justify-between text-base lg:text-xl">
                                 <div>
-                                    { word.ngoko } <span className="text-[0.5rem] text-white/50">(Ngoko)</span>
+                                    { word.ngoko } <span className="text-[0.5rem] text-white/50 lg:text-xs">(Ngoko)</span>
                                 </div>
                                 <div>
                                     <PiSpeakerHighBold />
                                 </div>
                             </div>
-                            <div className="flex justify-between text-base">
+                            <div className="flex justify-between text-base lg:text-xl">
                                 <div>
-                                    { word.madya } <span className="text-[0.5rem] text-white/50">(Krama Madya)</span>
+                                    { word.madya } <span className="text-[0.5rem] text-white/50 lg:text-xs">(Krama Madya)</span>
                                 </div>
                                 <div>
                                     <PiSpeakerHighBold />
                                 </div>
                             </div>
-                            <div className="flex justify-between text-base">
+                            <div className="flex justify-between text-base lg:text-xl">
                                 <div>
-                                    { word.alus } <span className="text-[0.5rem] text-white/50">(Krama Alus)</span>
+                                    { word.alus } <span className="text-[0.5rem] text-white/50 lg:text-xs">(Krama Alus)</span>
                                 </div>
                                 <div>
                                     <PiSpeakerHighBold />
