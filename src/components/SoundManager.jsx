c6a908a559a22bcec9fcd5d6e257f4a707a4f6c3
Filbert Fernandes <filbertfernandes.bot@gmail.com>
2024-08-25 12:49:51 +0700

    Change sounds volume

--- src/components/SoundManager.jsx
+++ src/components/SoundManager.jsx
@@ -3,11 +3,11 @@ import { Howl, Howler } from 'howler'
 
 // Sound files
 const sounds = {
-    move: new Howl({ src: ['./sounds/leaves01.mp3'], volume: 0.7 }),
+    move: new Howl({ src: ['./sounds/leaves01.mp3'], volume: 0.5 }),
     soccerBall: new Howl({ src: ['./sounds/soccer-ball.mp3'], volume: 0.1 }),
     buttonClick: new Howl({ src: ['./sounds/btn-click-sfx.mp3'], volume: 0.7 }),
-    freePhaseBackground: new Howl({ src: ['./sounds/bgm-2.mp3'], loop: true, volume: 0.025 }),
-    gamePhaseBackground: new Howl({ src: ['./sounds/bgm-3.mp3'], loop: true, volume: 0.025 })
+    freePhaseBackground: new Howl({ src: ['./sounds/bgm-2.mp3'], loop: true, volume: 0.03 }),
+    gamePhaseBackground: new Howl({ src: ['./sounds/bgm-3.mp3'], loop: true, volume: 0.03 })
 }
 
 let isMuted = false
@@ -30,7 +30,7 @@ export const SoundManager = {
             const sound = sounds.soccerBall
 
             if(sound) {
-                sound._volume = Math.random() * 0.1 + 0.1
+                sound._volume = Math.random() * 0.2 + 0.1
                 if(!sound.playing()) sound.play()
             }
         }
