a498e528c537ff193028276d3230f17acc471844
Filbert Fernandes <filbertfernandes.bot@gmail.com>
2024-10-13 20:35:41 +0700

    Add constants.js for third game

---
+++ components/home/games/third-game/stores/constants.js
@@ -0,0 +1,68 @@
+export const words = [
+  {
+    number: "0",
+    ngoko: "nol",
+    madya: "nol",
+    alus: "nol",
+  },
+  {
+    number: "1",
+    ngoko: "siji",
+    madya: "setunggal",
+    alus: "setunggal",
+  },
+  {
+    number: "2",
+    ngoko: "loro",
+    madya: "kalih",
+    alus: "kalih",
+  },
+  {
+    number: "3",
+    ngoko: "telu",
+    madya: "tiga",
+    alus: "tiga",
+  },
+  {
+    number: "4",
+    ngoko: "papat",
+    madya: "sekawan",
+    alus: "sekawan",
+  },
+  {
+    number: "5",
+    ngoko: "lima",
+    madya: "gangsal",
+    alus: "gangsal",
+  },
+  {
+    number: "6",
+    ngoko: "enem",
+    madya: "enem",
+    alus: "enem",
+  },
+  {
+    number: "7",
+    ngoko: "pitu",
+    madya: "pitu",
+    alus: "pitu",
+  },
+  {
+    number: "8",
+    ngoko: "wolu",
+    madya: "wolu",
+    alus: "wolu",
+  },
+  {
+    number: "9",
+    ngoko: "sanga",
+    madya: "sanga",
+    alus: "sanga",
+  },
+  {
+    number: "10",
+    ngoko: "sepuluh",
+    madya: "sedasa",
+    alus: "sedasa",
+  },
+]
