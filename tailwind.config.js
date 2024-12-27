6200e741da061faa5576870bc6513beed0b41ca5
filbertfernandes <filbertfernandes.bot@gmail.com>
2024-12-27 23:18:57 +0700

    Add specific preference user settings.json for this project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,62 +1,62 @@
 /** @type {import('tailwindcss').Config} */
-
-import daisyui from "daisyui"
+import daisyui from "daisyui";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			bebas: ["var(--font-bebas)"],
-  			questrial: ["var(--font-questrial)"]
-  		},
-  		animation: {
-  			bounceIn: 'bounceIn 0.5s ease-out',
-  			bounceInFadeOut: 'bounceInFadeOut 3s ease-out'
-  		},
-  		keyframes: {
-  			bounceIn: {
-  				'0%': {
-  					transform: 'scale(0.3)',
-  					opacity: '0'
-  				},
-  				'50%': {
-  					transform: 'scale(1.06)',
-  					opacity: '1'
-  				},
-  				'100%': {
-  					transform: 'scale(1)',
-  					opacity: '1'
-  				}
-  			},
-  			bounceInFadeOut: {
-  				'0%': {
-  					transform: 'scale(0.3)',
-  					opacity: '0'
-  				},
-  				'5%': {
-  					transform: 'scale(1)',
-  					opacity: '1'
-  				},
-  				'100%': {
-  					transform: 'scale(0)',
-  					opacity: '0'
-  				}
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      fontFamily: {
+        bebas: ["var(--font-bebas)"],
+        questrial: ["var(--font-questrial)"],
+      },
+      animation: {
+        bounceIn: "bounceIn 0.5s ease-out",
+        bounceInFadeOut: "bounceInFadeOut 3s ease-out",
+      },
+      keyframes: {
+        bounceIn: {
+          "0%": {
+            transform: "scale(0.3)",
+            opacity: "0",
+          },
+          "50%": {
+            transform: "scale(1.06)",
+            opacity: "1",
+          },
+          "100%": {
+            transform: "scale(1)",
+            opacity: "1",
+          },
+        },
+        bounceInFadeOut: {
+          "0%": {
+            transform: "scale(0.3)",
+            opacity: "0",
+          },
+          "5%": {
+            transform: "scale(1)",
+            opacity: "1",
+          },
+          "100%": {
+            transform: "scale(0)",
+            opacity: "0",
+          },
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
-  plugins: [daisyui, require("tailwindcss-animate")],
-}
+  plugins: [daisyui, tailwindcssAnimate],
+};
