bfe67865afbeb0e9c89fa0cbdb272943f7df6d9b
filbertfernandes <filbertfernandes.bot@gmail.com>
2025-02-22 11:04:48 +0700

    Adjust sound manager volume

--- lib/SoundManager.jsx
+++ lib/SoundManager.jsx
@@ -5,43 +5,43 @@ const sounds = {
   freePhaseBackground: new Howl({
     src: ["/sounds/bgm/bgm-1.mp3"],
     loop: true,
-    volume: 0.4,
+    volume: 0.2,
   }),
   gamePhaseBackground: new Howl({
     src: ["/sounds/bgm/bgm-2.mp3"],
     loop: true,
-    volume: 0.4,
+    volume: 0.1,
   }),
   move: new Howl({ src: ["/sounds/sfx/footstep-grass.mp3"], volume: 1.5 }),
   soccerBallImpact: new Howl({
     src: ["/sounds/sfx/soccer-ball.mp3"],
     volume: 1.5,
   }),
-  buttonClick: new Howl({ src: ["/sounds/sfx/btn-click.mp3"], volume: 0.7 }),
+  buttonClick: new Howl({ src: ["/sounds/sfx/btn-click.mp3"], volume: 1 }),
   correctAnswer: new Howl({
     src: ["/sounds/sfx/correct-answer.mp3"],
-    volume: 0.5,
+    volume: 1,
   }),
   wrongAnswer: new Howl({
     src: ["/sounds/sfx/wrong-answer.mp3"],
-    volume: 0.5,
+    volume: 1,
   }),
   gameComplete: new Howl({
     src: ["/sounds/sfx/game-complete.mp3"],
-    volume: 0.5,
+    volume: 1,
   }),
 
   // FIRST GAME
   keyboardType: new Howl({
     src: ["/sounds/sfx/keyboard-type.mp3"],
-    volume: 0.7,
+    volume: 1,
   }),
 
   // SECOND GAME
-  marblePush: new Howl({ src: ["/sounds/sfx/marble-push.mp3"], volume: 0.7 }),
+  marblePush: new Howl({ src: ["/sounds/sfx/marble-push.mp3"], volume: 1 }),
   marbleImpact: new Howl({
     src: ["/sounds/sfx/marble-impact.mp3"],
-    volume: 0.7,
+    volume: 1,
   }),
 };
 
