d490fa0ba287545eb687f1a6dda1cf3c2bbea148
filbertfernandes <filbertfernandes.bot@gmail.com>
2025-03-26 18:42:26 +0700

    Change ScoreTable.jsx UI

--- components/profile/ScoreTable.jsx
+++ components/profile/ScoreTable.jsx
@@ -24,28 +24,35 @@ const scoresData = [
 
 const ScoreTable = ({ scores }) => {
   return (
-    <div className="flex w-full flex-col items-center gap-4 rounded-xl border-2 bg-white/10 px-6 py-4">
+    <div className="flex w-full flex-col items-center gap-4 rounded-xl border-2 bg-white/10 px-10 py-8">
       <div className="flex items-center justify-center">
         <h6 className="h6-bold">Vocabulary Scores</h6>
         <TbVocabulary className="ml-1 text-xl" />
       </div>
-      {Object.entries(scores).map(([game, scoreCategories], index) => (
-        <div key={index} className="flex w-full flex-wrap items-center pb-2">
-          <h3 className="h6-bold">{scoresData[index].category}</h3>
-          <div className="ml-1 text-xl lg:text-3xl">
-            {scoresData[index].icon}
-          </div>
-          {Object.entries(scoreCategories).map(([category, score], i) => (
-            <div
-              key={i}
-              className="mt-1 flex w-full justify-between text-base sm:text-lg lg:text-xl"
-            >
-              <h6 className="capitalize">{category}</h6>
-              <h6>{score}</h6>
+      <div className="grid w-full grid-cols-1 gap-6 sm:grid-cols-2">
+        {Object.entries(scores).map(([game, scoreCategories], index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center rounded-lg border px-6 py-4"
+          >
+            <div className="flex w-full items-center">
+              <h3 className="h6-bold">{scoresData[index].category}</h3>
+              <div className="ml-1 text-xl lg:text-3xl">
+                {scoresData[index].icon}
+              </div>
             </div>
-          ))}
-        </div>
-      ))}
+            {Object.entries(scoreCategories).map(([category, score], i) => (
+              <div
+                key={i}
+                className="mt-1 flex w-full justify-between text-base sm:text-lg lg:text-xl"
+              >
+                <h6 className="capitalize">{category}</h6>
+                <h6>{score}</h6>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
