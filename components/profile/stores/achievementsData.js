6e1cbbe5322ffb3baedada577e89a991a8154a17
filbertfernandes <filbertfernandes.bot@gmail.com>
2025-03-29 17:13:28 +0700

    Move achievementsData

---
+++ components/profile/stores/achievementsData.js
@@ -0,0 +1,106 @@
+export const achievementsData = [
+  {
+    id: "curriculum-completion",
+    src: "/images/achievements/curriculum-completion.jpg",
+    description_english:
+      "Master the language by completing the entire curriculum.",
+    description_indonesian:
+      "Kuasai bahasa dengan menyelesaikan seluruh kurikulum.",
+  },
+  {
+    id: "top-3",
+    src: "/images/achievements/top-3.jpg",
+    description_english: "Achieve a top 3 position in the Vocabulary Game.",
+    description_indonesian:
+      "Capai peringkat 3 besar dalam Permainan Kosa Kata.",
+  },
+  {
+    id: "top-10",
+    src: "/images/achievements/top-10.jpg",
+    description_english: "Achieve a top 10 position in the Vocabulary Game.",
+    description_indonesian:
+      "Capai peringkat 10 besar dalam Permainan Kosa Kata.",
+  },
+  {
+    id: "top-50",
+    src: "/images/achievements/top-50.jpg",
+    description_english: "Achieve a top 50 position in the Vocabulary Game.",
+    description_indonesian:
+      "Capai peringkat 50 besar dalam Permainan Kosa Kata.",
+  },
+  {
+    id: "top-100",
+    src: "/images/achievements/top-100.jpg",
+    description_english: "Achieve a top 100 position in the Vocabulary Game.",
+    description_indonesian:
+      "Capai peringkat 100 besar dalam Permainan Kosa Kata.",
+  },
+  {
+    id: "correct-translations-1000",
+    src: "/images/achievements/correct-translations-1000.jpg",
+    description_english: "Achieve 1,000 correct translations.",
+    description_indonesian: "Capai 1.000 terjemahan yang benar.",
+  },
+  {
+    id: "correct-translations-500",
+    src: "/images/achievements/correct-translations-500.jpg",
+    description_english: "Achieve 500 correct translations.",
+    description_indonesian: "Capai 500 terjemahan yang benar.",
+  },
+  {
+    id: "correct-translations-250",
+    src: "/images/achievements/correct-translations-250.jpg",
+    description_english: "Achieve 250 correct translations.",
+    description_indonesian: "Capai 250 terjemahan yang benar.",
+  },
+  {
+    id: "correct-translations-100",
+    src: "/images/achievements/correct-translations-100.jpg",
+    description_english: "Achieve 100 correct translations.",
+    description_indonesian: "Capai 100 terjemahan yang benar.",
+  },
+  {
+    id: "correct-translations-25",
+    src: "/images/achievements/correct-translations-25.jpg",
+    description_english: "Achieve 25 correct translations.",
+    description_indonesian: "Capai 25 terjemahan yang benar.",
+  },
+  {
+    id: "correct-translations-1",
+    src: "/images/achievements/correct-translations-1.jpg",
+    description_english: "Achieve your first correct translation.",
+    description_indonesian: "Capai terjemahan benar pertamamu.",
+  },
+  {
+    id: "first-time-body-parts",
+    src: "/images/achievements/first-time-body-parts.jpg",
+    description_english:
+      "Complete the Body Parts Vocabulary Game for the first time.",
+    description_indonesian:
+      "Selesaikan Permainan Kosa Kata Bagian Tubuh untuk pertama kalinya.",
+  },
+  {
+    id: "first-time-colors",
+    src: "/images/achievements/first-time-colors.jpg",
+    description_english:
+      "Complete the Colors Vocabulary Game for the first time.",
+    description_indonesian:
+      "Selesaikan Permainan Kosa Kata Warna untuk pertama kalinya.",
+  },
+  {
+    id: "first-time-numbers",
+    src: "/images/achievements/first-time-numbers.jpg",
+    description_english:
+      "Complete the Numbers Vocabulary Game for the first time.",
+    description_indonesian:
+      "Selesaikan Permainan Kosa Kata Angka untuk pertama kalinya.",
+  },
+  {
+    id: "first-time-animals",
+    src: "/images/achievements/first-time-animals.jpg",
+    description_english:
+      "Complete the Animals Vocabulary Game for the first time.",
+    description_indonesian:
+      "Selesaikan Permainan Kosa Kata Hewan untuk pertama kalinya.",
+  },
+];
