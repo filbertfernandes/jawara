4cdb3393990bc4b720fd2eac360c8a48add2530d
filbertfernandes <filbertfernandes.bot@gmail.com>
2025-03-29 22:54:22 +0700

    Fix get achievements error

--- database/userAchievement.model.js
+++ database/userAchievement.model.js
@@ -3,7 +3,7 @@ import { Schema, models, model } from "mongoose";
 const UserAchievementSchema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true }, // Reference to User
-    achievementId: { type: String, required: true, unique: true }, // Unique achievement ID
+    achievementId: { type: String, required: true }, // Unique achievement ID
     earnedAt: { type: Date, default: Date.now }, // Timestamp when achievement was earned
     seen: { type: Boolean, default: false }, // Whether the user has seen the achievement notification
   },
