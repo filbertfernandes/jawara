2f00bc67aa2d8fe0853565f9c7c940fae7663f9b
filbertfernandes <filbertfernandes.bot@gmail.com>
2025-04-02 14:17:53 +0700

    Add screen rotation text

--- components/home/laptop/LaptopInterface.jsx
+++ components/home/laptop/LaptopInterface.jsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import { useEffect } from "react";
 
 import { useLaptop } from "./stores/useLaptop";
@@ -7,6 +8,8 @@ import { Slider } from "@/components/ui/slider";
 import { phases, useGame } from "@/hooks/useGame";
 
 const LaptopInterface = () => {
+  const t = useTranslations("Home");
+
   const { changePhase } = useGame((state) => ({
     changePhase: state.changePhase,
   }));
@@ -44,6 +47,9 @@ const LaptopInterface = () => {
       </div>
       <div className="fixed bottom-0 mb-20 flex h-12 w-full items-end justify-center">
         <div className="w-3/4 laptop-sm:w-1/2 lg:w-1/4">
+          <h6 className="h6-bold mb-4 text-center font-questrial text-gray-100">
+            {t("adjust_screen_rotation")}
+          </h6>
           <Slider
             defaultValue={[((screenRotation - 0.3) / (2.4 - 0.3)) * 100]}
             min={0}
